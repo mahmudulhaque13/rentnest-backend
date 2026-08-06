1e1606e9c2b0d1db2e18305eb5395cbae568afa1
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-08-06 07:23:29 +0600

    resolve ESM imports for Render deployment

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import app from "./app";
-import config from "./config";
+import app from "./app.js";
+import config from "./config/index.js";
 
 const PORT = config.port || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
